a2de02a0e6a5186e535661e4498fc648e59ef2d7
Oliver Wahlen <oliver.wahlen@infinit-group.de>
2023-10-06 17:11:17 +0200

    added support for heart beats

--- src/main/kotlin/org/wahlen/springwebsocket/configuration/WebSocketConfig.kt
+++ src/main/kotlin/org/wahlen/springwebsocket/configuration/WebSocketConfig.kt
@@ -1,7 +1,10 @@
 package org.wahlen.springwebsocket.configuration
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.scheduling.TaskScheduler
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
@@ -14,7 +17,10 @@ class WebSocketConfig : WebSocketMessageBrokerConfigurer {
     override fun configureMessageBroker(config: MessageBrokerRegistry) {
         // simple memory-based message broker to carry the greeting messages
         // back to the client on destinations prefixed with /topic
+        // The taskScheduler is optionally configured and allows clients to request heartbeats
+        // with the STOMP CONNECT frame.
         config.enableSimpleBroker("/topic")
+            .setTaskScheduler(heartBeatScheduler())
 
         // designates the /app prefix for messages that are bound for methods annotated with @MessageMapping
         // For example, /app/hello is the endpoint that the GreetingController.greeting() method is mapped to handle.
@@ -26,5 +32,10 @@ class WebSocketConfig : WebSocketMessageBrokerConfigurer {
         val registration = registry.addEndpoint("/gs-guide-websocket")
         // fixme: CORS should be disabled in a production environment (prod profile)
         registration.setAllowedOrigins("*")
+    }
+
+    @Bean
+    fun heartBeatScheduler(): TaskScheduler {
+        return ThreadPoolTaskScheduler()
     }
 }
