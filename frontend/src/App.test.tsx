b1969849d052b0f0e230503a05223348fa831548
Oliver Wahlen <oliver.wahlen@infinit-group.de>
2023-10-08 01:39:30 +0200

    fixed tests and deployment

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -3,9 +3,9 @@ import {render, screen} from '@testing-library/react'
 import {App} from './App'
 
 describe('App component', () => {
-    it('should render "Hello WebSocket', () => {
+    it('should render "WebSocket State', () => {
         render(<App/>)
-        const textElement = screen.getByText(/Hello WebSocket!/i)
+        const textElement = screen.getByText(/WebSocket State/i)
         expect(textElement).toBeInTheDocument()
     })
 })
