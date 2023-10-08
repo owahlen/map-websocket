6b532f4fbb7a6283e22ad8a3cbdede8e7195488e
Oliver Wahlen <oliver.wahlen@infinit-group.de>
2023-10-08 20:19:29 +0200

    bugfixes in Scheduler Config

--- src/main/kotlin/org/wahlen/mapwebsocket/configuration/WebSocketConfig.kt
+++ src/main/kotlin/org/wahlen/mapwebsocket/configuration/WebSocketConfig.kt
@@ -4,15 +4,12 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.scheduling.TaskScheduler
-import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
-
 @Configuration
-@EnableScheduling
 @EnableWebSocketMessageBroker // enable WebSocket message handling, backed by a message broker
 class WebSocketConfig : WebSocketMessageBrokerConfigurer {
 
